9d3928787016d179e275c4df0d70d44040661e75
DiveshMahajan <diveshmahajan04@gmail.com>
2024-03-30 01:27:03 +0530

    admin dashboard added

--- src/App.jsx
+++ src/App.jsx
@@ -17,6 +17,8 @@ import SignUp from './pages/SignUpPage';
 import ResetPassword from './pages/ResetPassword';
 import BrowseClientList from './pages/BrowseClientList';
 import FindExperts from './pages/FindExperts';
+import AdminPage from './pages/AdminPage';
+import Requests from './pages/Requests';
 
 function App() {
 
@@ -42,11 +44,14 @@ function App() {
       <Routes>
         <Route exact path="/" element={<MainRender />} />
         <Route path="/find" element={<FindExperts />} />
+        <Route path="/admin" element={<AdminPage />} />
         <Route path='/browse' element={<BrowseClientList/>}/>
         <Route path="/profile" element={<ClerkProfile />} />
         <Route path="/signin" element={<SignIn />} />
         <Route path="/signup" element={<SignUp />} />
         <Route path="/reset-password" element={<ResetPassword />} />
+        <Route path='/admin/requests' element={<Requests/>}/>
+        <Route path='/admin/pendingRequests' element={<Requests/>}/>
       </Routes>
     </>
   );
